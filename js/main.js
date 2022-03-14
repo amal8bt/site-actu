03fdd1f0aefdbafcc44db75a7564af3c25f087fd
amal <bentalebamal8@gmail.com>
2022-03-14 09:52:23 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+currentDay = ()=>{
+    const options= { 
+        weekday: 'long', 
+        month: 'long',
+        day: 'numeric'
+    };
+    const today = new Date();
+    return today.toLocaleDateString("en-US",options);
+}
